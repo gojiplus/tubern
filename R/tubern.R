4e0bf2bb8ce2d6c0bc4f7ad16893eb9cd6c7d27e
***** <gsood07@gmail.com>
2016-11-08 17:16:00 -0500

    base functions

---
+++ R/tubern.R
@@ -0,0 +1,62 @@
+#' \pkg{tubern} provides access to the YouTube Analytics and Reporting API
+#'
+#' @name tubern
+#' @importFrom httr GET POST authenticate config stop_for_status upload_file content oauth_endpoints oauth_app oauth2.0_token
+#' @docType package
+NULL
+
+#' Check if authentication token is in options
+#'
+
+yt_check_token <- function() {
+
+	app_token = getOption('google_token')
+    if (is.null(app_token)) stop("Please get a token using yt_oauth()")
+
+}
+
+#' 
+#' Base POST AND GET functions. Not exported.
+
+#'
+#' GET
+#' 
+#' @param path path to specific API request URL 
+#' @param query query list 
+#' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
+#' @return list
+
+tubern_GET <- 
+function(path, query, ...) {
+
+	yt_check_token()
+
+	req <- GET("https://youtubereporting.googleapis.com/v1/", path= path, query=query, config(token = getOption("google_token")), ...)
+	
+	stop_for_status(req)
+	res <- content(req)
+
+	res
+}
+
+#'
+#' POST
+#' 
+#' @param path path to specific API request URL 
+#' @param query query list
+#' @param body passing image through body 
+#' @param \dots Additional arguments passed to \code{\link[httr]{GET}}.
+#' 
+#' @return list
+
+tubern_POST <- 
+function(path, query, body="", ...) {
+
+	yt_check_token()
+	
+	req <- POST("https://www.googleapis.com", path= path, body=body, query=query, config(token = getOption("google_token")), ...)
+	stop_for_status(req)
+	res <- content(req)
+
+	res
+}
